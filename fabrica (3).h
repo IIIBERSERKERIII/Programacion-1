ebe9a759f91687040d6bef01d1c64cec3951932e
IIIBERSERKERIII <danny.viver@udla.edu.ec>
2026-06-04 19:40:20 -0500

    Proyecto #2.1
    final final

---
+++ fabrica (3).h
@@ -0,0 +1,17 @@
+#ifndef FABRICA_H
+#define FABRICA_H
+
+#include <stdio.h>
+#include <string.h>
+#include "validaciones.h" 
+
+#define MAX_PROD 5
+#define MAX_CHARS 50
+
+int buscar(char n[][MAX_CHARS], int total, char query[]);
+void registrar(char n[][MAX_CHARS], int *c, float *t, float *r, int *total);
+void editar(char n[][MAX_CHARS], int *c, float *t, float *r, int total);
+void eliminar(char n[][MAX_CHARS], int *c, float *t, float *r, int *total);
+void calcular(int *c, float *t, float *r, int total);
+
+#endif
---
+++ validaciones (1).h
@@ -0,0 +1,7 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+int validar_int(char mensaje[]);
+float validar_float(char mensaje[]);
+
+#endif
